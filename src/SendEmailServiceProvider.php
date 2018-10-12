bf06731d28b91b6683c57cb9a8b0ed9951fe2688
DiemND <diem.ks95@gmail.com>
2018-10-12 09:42:55 +0700

    init src

---
+++ src/SendEmailServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Megaads\Sendemail;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratesitemapServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
